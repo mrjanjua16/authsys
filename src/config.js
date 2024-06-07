ca8372a58f61d9b4197056c203fed90ac61ebc61
Mubashir Ahmad <imubashirahmad@gmail.com>
2024-06-07 07:46:53 +0000

    JWT added

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 const connect = mongoose.connect(
-  'mongodb+srv://authsys:MgySeHlXYxBE26WR@cluster0.s5llm4l.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0.users'
+  process.env.MONGODB_URI
 );
 
 connect
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const collection = require('./config');
 
 const PORT = 5000;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -11,6 +15,8 @@ const app = express();
 app.use(express.json());
 // Parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
+// Use cookie-parser middleware
+app.use(cookieParser());
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -20,6 +26,18 @@ app.set('views', path.join(__dirname, '../views'));
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
+
+// JWT Middleware
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
 
 app.get("/", (req, res) => {
     res.render("login");
@@ -31,53 +49,55 @@ app.get("/signup", (req, res) => {
 
 app.post("/signup", async (req, res) => {
     try {
-
         console.log('Received signup data:', req.body);
 
-        const hashedPassword = await bcrypt.hash(req.body.password, 10); // Hash the password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
         const data = {
             name: req.body.username,
             password: hashedPassword
         };
 
-        const existingUser = await collection.findOne({name: data.name});;
+        const existingUser = await collection.findOne({ name: data.name });
 
-        if(existingUser)
-            {
-                res.send("User already exists. Please choose a different username.");
-            } else {
-                const userdata = await collection.create(data); // Use create instead of insertMany for a single document
-                console.log('User data inserted:', userdata);
-                res.redirect("/"); // Redirect after successful signup
-            }
+        if (existingUser) {
+            res.send("User already exists. Please choose a different username.");
+        } else {
+            const userdata = await collection.create(data);
+            console.log('User data inserted:', userdata);
+            const token = jwt.sign({ name: data.name }, JWT_SECRET, { expiresIn: '1h' });
+
+            // Send token as HTTP-only cookie
+            res.cookie('jwt', token, { httpOnly: true, secure: true, maxAge: 3600000 }); // 1 hour expiration
+            res.redirect("/home");
+        }
     } catch (error) {
         console.error('Error during signup:', error.message);
         res.status(500).send('Internal Server Error');
     }
 });
 
-app.post("/login", async (req, res) => 
-    {
-        try
-        {
-            const check = await collection.findOne({name: req.body.username});
-            if(!check)
-                {
-                    res.send("user name cannot found");
-                }
-            const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
-            if(isPasswordMatch)
-                {
-                    res.render("home");
-                }else{
-                    req.send("Wrong Password");
-                }
-        }catch{
-            res.send("Incorrect details");
+app.post("/login", async (req, res) => {
+    try {
+        const user = await collection.findOne({ name: req.body.username });
+        if (!user) {
+            return res.send("Username not found");
         }
-})
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+        if (isPasswordMatch) {
+            const token = jwt.sign({ name: user.name }, JWT_SECRET, { expiresIn: '1h' });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`);
+            // Send token as HTTP-only cookie
+            res.cookie('jwt', token, { httpOnly: true, secure: true, maxAge: 3600000 }); // 1 hour expiration
+            res.redirect("/home");
+        } else {
+            res.send("Wrong Password");
+        }
+    } catch (error) {
+        res.send("Incorrect details");
+    }
 });
+
+// Protected route example
+app.get("/home", authenticateToken, (req, res) => {
+    res.render("home", { user: req.user })})
